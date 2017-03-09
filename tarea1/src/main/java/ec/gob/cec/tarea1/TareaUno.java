66ee9a898a725dd6966c1ff1f62b49448533125d
Rolando Casigna <rolo4336@gmail.com>
2017-03-08 23:18:01 -0500

    desde aqui

---
+++ tarea1/src/main/java/ec/gob/cec/tarea1/TareaUno.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.gob.cec.tarea1;
+
+/**
+ *
+ * @author rcasigna
+ */
+public class TareaUno {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
